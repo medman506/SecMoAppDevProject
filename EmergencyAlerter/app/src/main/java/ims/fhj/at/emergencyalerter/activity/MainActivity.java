14c5773505d4347f45f24d5f16ddfd006aa8091c
MICSTI <stifter.michael@gmx.net>
2016-11-11 14:51:33 +0100

    Change minSdkVersion to 21, change app base theme

--- EmergencyAlerter/app/src/main/java/ims/fhj/at/emergencyalerter/activity/MainActivity.java
+++ EmergencyAlerter/app/src/main/java/ims/fhj/at/emergencyalerter/activity/MainActivity.java
@@ -1,16 +1,20 @@
 package ims.fhj.at.emergencyalerter.activity;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import ims.fhj.at.emergencyalerter.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnToMapActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +22,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // set title bar to a red color
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.flamingo)));
+        //getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.flamingo)));
 
-        // also set status bar color to red
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(getResources().getColor(R.color.tall_poppy));
-        }
+        // button to navigate to map activity
+        btnToMapActivity = (Button) findViewById(R.id.to_map_activity);
+        btnToMapActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mapActivityIntent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(mapActivityIntent);
+            }
+        });
     }
 }
