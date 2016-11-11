54d569a3ca864451761b1475d6aae972fdd66c38
medman506 <mayflc07@gmail.com>
2016-11-11 20:25:07 +0100

    Basic Layout and Activities; Buttons in the MainActivity

---
+++ EmergencyAlerter/app/src/main/java/ims/fhj/at/emergencyalerter/activity/SettingsActivity.java
@@ -0,0 +1,15 @@
+package ims.fhj.at.emergencyalerter.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ims.fhj.at.emergencyalerter.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
