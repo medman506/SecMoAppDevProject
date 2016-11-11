fbb483a58f67352ab1019e773cedae61b94659ab
MICSTI <stifter.michael@gmx.net>
2016-11-11 18:45:42 +0100

    Add connection establishment process to map activity

--- EmergencyAlerter/app/src/main/java/ims/fhj/at/emergencyalerter/activity/MapActivity.java
+++ EmergencyAlerter/app/src/main/java/ims/fhj/at/emergencyalerter/activity/MapActivity.java
@@ -1,15 +1,26 @@
 package ims.fhj.at.emergencyalerter.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
 
 import ims.fhj.at.emergencyalerter.R;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    public static String TAG = "MapActivity";
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,15 @@ public class MapActivity extends AppCompatActivity {
         // set action bar title
         getSupportActionBar().setTitle(getResources().getString(R.string.nearby_police_stations));
 
+        // set up Google API client
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,4 +51,30 @@ public class MapActivity extends AppCompatActivity {
         });*/
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "Connection to Google Places API established");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "Connection to Google Places API suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e(TAG, "Connection to Google Places API failed");
+    }
 }
