380f996e481cf3f2bf06aae368d0953dac59dd31
MICSTI <stifter.michael@gmx.net>
2016-11-11 14:25:05 +0100

    Change color of status bar to dark red

--- EmergencyAlerter/app/src/main/java/ims/fhj/at/emergencyalerter/activity/MainActivity.java
+++ EmergencyAlerter/app/src/main/java/ims/fhj/at/emergencyalerter/activity/MainActivity.java
@@ -1,8 +1,12 @@
 package ims.fhj.at.emergencyalerter.activity;
 
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import ims.fhj.at.emergencyalerter.R;
 
@@ -15,5 +19,12 @@ public class MainActivity extends AppCompatActivity {
 
         // set title bar to a red color
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.flamingo)));
+
+        // also set status bar color to red
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.tall_poppy));
+        }
     }
 }
