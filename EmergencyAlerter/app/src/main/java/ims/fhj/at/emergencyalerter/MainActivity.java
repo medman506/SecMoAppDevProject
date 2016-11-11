43600399d54d28c3d4f5354e1e59940f8e425faa
MICSTI <stifter.michael@gmx.net>
2016-11-11 12:06:54 +0100

    Set action bar background color to red

--- EmergencyAlerter/app/src/main/java/ims/fhj/at/emergencyalerter/MainActivity.java
+++ EmergencyAlerter/app/src/main/java/ims/fhj/at/emergencyalerter/MainActivity.java
@@ -1,5 +1,6 @@
 package ims.fhj.at.emergencyalerter;
 
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set title bar to a red color
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.flamingo)));
     }
 }
